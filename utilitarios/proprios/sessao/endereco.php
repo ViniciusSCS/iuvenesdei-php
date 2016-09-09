65c1ee7225afdb14640220a6d8dab43af2917c94
Vinicius Sarmento <vinicius.siqueira@unievangelica.edu.br>
2016-09-09 15:31:40 -0300

    Alterações na fonte do endereço.

--- utilitarios/proprios/sessao/endereco.php
+++ utilitarios/proprios/sessao/endereco.php
@@ -3,8 +3,11 @@
         <section id="local" class="contact-parlex">                        
             <div class="map">
                 <h3 id="titulo" style="font-size: 25px;">Local:</h3>
-                <h4 id="titulo" style="font-size: 15px;">R. Don&aacute; Ada Centini, 985 - Maracan&atilde;, An&aacute;polis - GO, 75040-050,
-                    Par&oacute;quia S&atilde;o Pedro e S&atilde;o Paulo</h4>
+                <h4 id="endereco" style="font-size: 15px;">
+                    <b>R. Don&aacute; Ada Centini, 985 - Maracan&atilde;,
+                    An&aacute;polis - GO, 75040-050,
+                    Par&oacute;quia S&atilde;o Pedro e S&atilde;o Paulo</b>
+                </h4>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3829.2486492833577!2d-48.96034858561605!3d-16.310234540197456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ea3f61e33f893%3A0xf9e2ffda428e0467!2sIgreja+S%C3%A3o+Pedro+e+S%C3%A3o+Paulo!5e0!3m2!1spt-BR!2sbr!4v1453395298354" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
             <a href="https://www.facebook.com/iuvenes.dei/"><i class="fa fa-facebook-official fa-4x"></i></a>                            
