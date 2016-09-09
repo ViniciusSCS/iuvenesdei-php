f272986165f5e40a91cdebca2d4b570b1541c2df
Vinicius Sarmento <vinicius.siqueira@unievangelica.edu.br>
2016-09-09 15:13:07 -0300

    Colocando link na imagem do menu

--- utilitarios/proprios/sessao/menu.php
+++ utilitarios/proprios/sessao/menu.php
@@ -9,7 +9,7 @@
                             <span class="icon-bar"></span> 
                             <span class="icon-bar"></span> 
                         </button>
-                        <img class="img-responsive" src="utilitarios/imagens/icone/icone.png" alt="" />
+                        <a href="index"><img class="img-responsive" src="utilitarios/imagens/icone/icone.png" alt="" /></a>
                     </div>
                     <!--/.navbar-header-->
                     <div id="main-nav" class="collapse navbar-collapse">
