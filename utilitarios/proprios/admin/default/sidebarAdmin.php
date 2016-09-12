bd12c9e858af22a1537d00b83233cd9270819090
Vinicius Sarmento <vinicius.siqueira@unievangelica.edu.br>
2016-09-12 17:30:41 -0300

    Organizando

---
+++ utilitarios/proprios/admin/default/sidebarAdmin.php
@@ -0,0 +1,56 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="../utilitarios/externos/admin/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>Usuario</p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu">
+      <li class="header">Navegação</li>
+      <li id="principal" class="treeview">
+        <a href="principal">
+          <i class="fa fa-dashboard"></i> <span>Principal</span> <i class="fa fa-angle-left pull-right"></i>
+        </a>
+      </li>
+
+      <li id="eventos" class="treeview">
+        <a href="eventos">
+          <i class="fa fa-calendar"></i> <span>Eventos</span> <i class="fa fa-angle-left pull-right"></i>
+        </a>
+      </li>
+
+      <li id="album" class="treeview">
+        <a href="album">
+          <i class="fa fa-book"></i> <span>Albuns</span> <i class="fa fa-angle-left pull-right"></i>
+        </a>
+      </li>
+
+      <li id="comentarios" class="treeview">
+        <a href="comentario.php">
+          <i class="fa fa-comments"></i> <span>Comentários</span> <i class="fa fa-angle-left pull-right"></i>
+        </a>
+      </li>
+
+      <!--
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-files-o"></i>
+          <span>Layout Options</span>
+          <span class="label label-primary pull-right">4</span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+        </ul>
+      </li>
+      -->
+    </ul>
+  </section>
+</aside>
+  <!-- /.sidebar -->
---
+++ utilitarios/proprios/admin/layout/principal.php
@@ -0,0 +1,20 @@
+<?php 
+    include_once '../default/headerAdmin.php';
+    include_once '../default/sidebarAdmin.php'
+?>
+<!-- Content Wrapper. Contains page content -->
+    <section id="principal">
+        <div class="content-wrapper">
+            <input type="hidden" id="pagina" value="principal">
+            <section class="content-header">
+                <h1>
+                    Principal
+                </h1>
+            </section>
+            <section class="content">
+                iugoiutou
+            </section>
+        </div>
+    </section>
+
+<?php include '../default/footerAdmin.php' ?>
