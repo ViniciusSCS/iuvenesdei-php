d57a184d820b623a2f2584f408913fb73ba0ad54
Vinicius Sarmento <vinicius.siqueira@unievangelica.edu.br>
2016-09-14 17:37:00 -0300

    novo bd

--- bd/iuvenesd_iuvenesdei.sql
+++ bd/iuvenesd_iuvenesdei.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-Set-2016 às 16:46
+-- Generation Time: 14-Set-2016 às 22:32
 -- Versão do servidor: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -31,15 +31,24 @@ CREATE TABLE IF NOT EXISTS `login` (
   `nome` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `usuario` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `senha` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sexo` int(11) NOT NULL,
+  `situacao` int(11) NOT NULL,
   PRIMARY KEY (`idLogin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Extraindo dados da tabela `login`
 --
 
-INSERT INTO `login` (`idLogin`, `nome`, `usuario`, `senha`) VALUES
-(1, 'Vinicius Sarmento', 'vinicius.sarmento', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `login` (`idLogin`, `nome`, `usuario`, `senha`, `sexo`, `situacao`) VALUES
+(1, 'Vinicius Sarmento', 'vinicius.sarmento', 'e10adc3949ba59abbe56e057f20f883e', 1, 1),
+(2, 'Aline Ribeiro', 'aline.ribeiro', '1467c0187371ec54c37a87b1c788454e', 0, 1),
+(3, 'Beatriz Miranda', 'beatriz.miranda', '7397e8a96241e5a576cdc71ce2904e8a', 0, 1),
+(4, 'Pedro Henrique', 'pedro.henrique', '7d9cd6ce6752fe8ea5c076a27f1d7034', 1, 1),
+(5, 'Edgar Junio', 'edgar.junio', '3ed479143a9e4372c6fc9f44be574432', 1, 1),
+(6, 'Juliana Rodrigues', 'juliana.rodrigues', 'ff69dbb734c3a1409e0549fb73c9be90', 0, 1),
+(7, 'Juliana Rezende', 'juliana.rezende', '5f06da6553247ed72ecb379b7213c902', 0, 1),
+(8, 'Thiago Batista', 'thiago.batista', 'a156a0bd45f9285490d59ef7389e1dbc', 1, 1);
 
 -- --------------------------------------------------------
 
