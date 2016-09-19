9e6e8467504f2000d6548350d97bea62007e465d
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2016-09-19 19:40:51 -0300

    Alteração

--- index.php
+++ index.php
@@ -3,19 +3,11 @@
 require 'persistencia/Conexao.class.php';
 require 'persistencia/ComentsDAO.class.php';
 require 'persistencia/ComentsVO.class.php';
-//require 'persistencia/EventoDAO.class.php';
-//require 'persistencia/EventoVO.class.php';
 
 $comentsDAO = new ComentsDAO();
 $formComents = $comentsDAO->buscarComents();
 $total = count($formComents);
 
-
-
-
-//$eventoDAO = new EventoDAO;
-//$formEvento = $eventoDAO->buscaEvento();
-//$qnt = count($formEvento);
 
 
 include_once './utilitarios/proprios/sessao/header.php';
